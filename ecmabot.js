37309adf5bc9b2066edcf459489fb2ea235447c8
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-06-25 16:51:12 -0700

    change trigger to babelbot

--- ecmabot.js
+++ ecmabot.js
@@ -20,7 +20,7 @@ var JSBot = function(profile) {
 
 	Bot.call(this, profile);
 	this.set_log_level(this.LOG_ALL);
-	this.set_trigger("!"); // Exclamation
+	this.set_trigger("babelbot") // Exclamation
 };
 
 
